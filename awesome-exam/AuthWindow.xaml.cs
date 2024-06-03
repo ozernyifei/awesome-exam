be481756485a48da091791b20e78e79955b20f98
pashastaf <pashastaf1@gmail.com>
2024-06-04 00:05:59 +0300

    final cut

---
+++ awesome-exam/AuthWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace awesome_exam
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            var UAuth = ClassHelper.EF.Context.User.ToList().Where(i => i.Login == TbLogin.Text && i.Password == PbPassword.Password).FirstOrDefault();
+            var EmpAuth = ClassHelper.EF.Context.Employee.ToList().Where(i => i.Login == TbLogin.Text && i.Password == PbPassword.Password).FirstOrDefault();
+            if (UAuth != null)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else if (EmpAuth != null)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Пользователя не существует", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
+    }
+}
--- awesome-exam/MainWindow.xaml.cs
+++ awesome-exam/MainWindow.xaml.cs
@@ -23,11 +23,72 @@ namespace awesome_exam
         public MainWindow()
         {
             InitializeComponent();
+            GetList();
+            LoadComboBox();
+        }
+        private void GetList()
+        {
+            DataGrid.ItemsSource = ClassHelper.EF.Context.UserOrderDataView.ToList();
         }
 
-        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        private void LoadComboBox()
         {
+            ComboBoxFullName.ItemsSource = ClassHelper.EF.Context.UserOrderDataView.Select(u => u.FullName).Distinct().ToList();
+            ComboBoxFullName.SelectedIndex = 0;
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedFullName = ComboBoxFullName.SelectedItem as string;
+            DataGrid.ItemsSource = ClassHelper.EF.Context.UserOrderDataView
+                                    .Where(u => u.FullName == selectedFullName)
+                                    .ToList();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            GetList();
+        }
+
+        private void SortData_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedColumn = ComboBoxSortColumn.SelectedItem as ComboBoxItem;
+            bool ascending = RadioButtonAscending.IsChecked == true;
+            bool descending = RadioButtonDescending.IsChecked == true;
+
+            if (selectedColumn == null || (!ascending && !descending))
+            {
+                MessageBox.Show("Пожалуйста, выберите столбец и направление сортировки.");
+                return;
+            }
+
+            var data = ClassHelper.EF.Context.UserOrderDataView.ToList();
+
+            switch (selectedColumn.Content.ToString())
+            {
+                case "Дата":
+                    if (ascending)
+                        data = data.OrderBy(u => u.SaleDate).ToList();
+                    else if (descending)
+                        data = data.OrderByDescending(u => u.SaleDate).ToList();
+                    break;
+                case "Статус":
+                    if (ascending)
+                        data = data.OrderBy(u => u.Title).ToList();
+                    else if (descending)
+                        data = data.OrderByDescending(u => u.Title).ToList();
+                    break;
+                case "ФИО":
+                    if (ascending)
+                        data = data.OrderBy(u => u.FullName).ToList();
+                    else if (descending)
+                        data = data.OrderByDescending(u => u.FullName).ToList();
+                    break;
+            }
+
+            DataGrid.ItemsSource = data;
         }
     }
 }
+
