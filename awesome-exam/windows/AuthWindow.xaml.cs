c16855e60327f84bf27ffdb0fad93b1d5ab21a64
Toadles <a.lyabin@sdksys.ru>
2024-06-03 23:34:27 +0300

    kek

--- awesome-exam/windows/AuthWindow.xaml.cs
+++ awesome-exam/windows/AuthWindow.xaml.cs
@@ -19,14 +19,37 @@ namespace awesome_exam.windows
     /// </summary>
     public partial class AuthWindow : Window
     {
+        DB.Employee employee = null;
+        DB.User user = null;
+
         public AuthWindow()
         {
             InitializeComponent();
         }
 
-        private void btnSignIn_Click(object sender, RoutedEventArgs e)
-        {
 
+        private void btnSignIn_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PbPassword.Password) && string.IsNullOrEmpty(TbLogin.Text))
+            {
+                employee = helper.EF.Context.Employees.ToList().Where(i => i.Password == PbPassword.Password && i.Login == TbLogin.Text).FirstOrDefault();
+                user = helper.EF.Context.Users.ToList().Where(i => i.Password == PbPassword.Password && i.Login == TbLogin.Text).FirstOrDefault();
+
+                if (user != null)
+                {
+                    ClientWindow client = new ClientWindow();
+                }
+
+                else if (employee != null)
+                {
+                    ClientWindow client = new ClientWindow();
+                }
+                else
+                {
+                    MessageBox.Show("Пользователя не существует", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+           
         }
     }
 }
--- awesome-exam/windows/ClientWindow.xaml.cs
+++ awesome-exam/windows/ClientWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using awesome_exam.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,14 @@ namespace awesome_exam.windows
         public ClientWindow()
         {
             InitializeComponent();
+            GetList();
         }
+
+        private void GetList()
+        {
+            dg_DataGrid.ItemsSource = helper.EF.Context.UserOrderDataViews.ToList();
+        }
+
+        
     }
 }
